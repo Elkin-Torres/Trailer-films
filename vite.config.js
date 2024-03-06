e4a2e44fd629a8940fbd4b87ca16f4900a734f58
Elkin <elkin148@hotmail.com>
2024-03-05 22:44:07 -0500

    establishing distribution

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://elkin-Torres.github.io/Trailer-films/"
+  base:"https://elkin-Torres.github.io/Trailer-films/",
 })
